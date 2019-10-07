81c92b19869214a669946c69103bdcdfd3688096
Anton <weather51nomad@gmail.com>
2019-10-07 20:44:32 +0700

    add masses
    Code for adding masses with you mouse

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ class nBody {
 
             massI.x += massI.vx * this.dt;
             massI.y += massI.vy * this.dt;
-            massI.z += massI.vz * this.dt;
+           
         }
 
         return this;
@@ -35,7 +35,7 @@ class nBody {
 
             massI.vx += massI.ax * this.dt;
             massI.vy += massI.ay * this.dt;
-            massI.vz += massI.az * this.dt;
+         
         }
     }
 
@@ -48,7 +48,7 @@ class nBody {
         for (let i = 0; i < massesLen; i++) {
             let ax = 0;
             let ay = 0;
-            let az = 0;
+           
 
             const massI = this.masses[i];
 
@@ -58,21 +58,21 @@ class nBody {
 
                     const dx = massJ.x - massI.x;
                     const dy = massJ.y - massI.y;
-                    const dz = massJ.z - massI.z;
+                    
 
-                    const distSq = dx * dx + dy * dy + dz * dz;
+                    const distSq = dx * dx + dy * dy ;
 
                     const f = (this.g * massJ.m) / (distSq * Math.sqrt(distSq + this.softeningConstant));
 
                     ax += dx * f;
                     ay += dy * f;
-                    az += dz * f;
+                  
                 }
             }
 
             massI.ax = ax;
             massI.ay = ay;
-            massI.az = az;
+            
             }
             return this;
         }
@@ -93,50 +93,41 @@ const masses = [{
     m: 1,
     x: -1.50324727873647e-6,
     y: -3.93762725944737e-6,
-    z: -4.86567877183925e-8,
     vx: 3.1669325898331e-5,
-    vy: -6.85489559263319e-6,
-    vz: -7.90076642683254e-7
+    vy: -6.85489559263319e-6, 
 },
 {
     name: "Mercury",
     m: 1.65956463e-7,
     x: -0.346390408691506,
     y: -0.272465544507684,
-    z: 0.00951633403684172,
     vx: 4.25144321778261,
     vy: -7.61778341043381,
-    vz: -1.01249478093275
   },
   {
     name: "Venus",
     m: 2.44699613e-6,
     x: -0.168003526072526,
     y: 0.698844725464528,
-    z: 0.0192761582256879,
     vx: -7.2077847105093,
     vy: -1.76778886124455,
-    vz: 0.391700036358566
+   
   },
   {
     name: "Earth",
     m: 3.0024584e-6,
     x: 0.648778995445634,
     y: 0.747796691108466,
-    z: -3.22953591923124e-5,
     vx: -4.85085525059392,
     vy: 4.09601538682312,
-    vz: -0.000258553333317722
   },
   {
     name: "Mars",
     m: 3.213e-7,
     x: -0.574871406752105,
     y: -1.395455041953879,
-    z: -0.01515164037265145,
     vx: 4.9225288800471425,
     vy: -1.5065904473191791,
-    vz: -0.1524041758922603
   }
 ];
 
@@ -202,7 +193,7 @@ class Visualization {
     
 }
 
-const canvas = document.querySelector("#canvas");
+const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
@@ -220,9 +211,60 @@ const populateVisualization = masses => {
 
 populateVisualization (innerSolarSystem.masses);
 
-document.querySelector('#reset-button').addEventListener('click', () => {
+document.querySelector('.reset-button').addEventListener('click', () => {
     innerSolarSystem.masses = JSON.parse(JSON.stringify(masses));
     populateVisualization(innerSolarSystem.masses);
+}, false);
+
+//Добавление небесных тел в симуляцию пользователем
+
+//переменные, которые будут хранить координаты мышки
+let mousePressX = 0;
+let mousePressY = 0;
+
+//переменные, которые хранят координаты текущего положения мышки
+let currentMouseX = 0;
+let currentMouseY = 0;
+
+//переменную, которая хранит состояние: перемещается мышь или нет
+let dragging = false;
+
+//событие записывающий координаты того места, где была нажата мышь,
+//и устанавливает переменную перемещения в значение true.
+canvas.addEventListener("mousedown", e => {
+        mousePressX = e.clientX;
+        mousePressY = e.clientY;
+        dragging = true;
+    },
+    false
+);
+
+//событие записывающие координаты текущего положения курсора мыши
+canvas.addEventListener("mousemove", e => {
+        currentMouseX = e.clientX;
+        currentMouseY = e.clientY;
+    }, 
+    false
+);
+
+const massesList = document.querySelector(".masses-list");
+
+canvas.addEventListener("mouseup", e => {
+    const x = (mousePressX - width / 2) / scale;
+    const y = (mousePressY - height / 2) / scale;
+    const vx = (e.clientX - mousePressX) / 500;
+    const vy = (e.clientY - mousePressY) / 500;
+
+    innerSolarSystem.masses.push({
+        m: parseFloat(massesList.value),
+        x,
+        y,
+        vx,
+        vy,
+        visualization: new Visualization(ctx, trailLength, radius)
+    });
+
+    dragging = false;
 }, false);
 
 const animate = () => {
@@ -249,6 +291,21 @@ const animate = () => {
             ctx.fillText(massI.name, x + 12, y + 4);
             ctx.fill();
         }
+
+        if (x < radius || x > width - radius) {
+            massI.vx = -massI.vx;
+        }
+        if (y < radius || y > height - radius) {
+            massI.vy = -massI.vy;
+        }
+    }
+    
+    if (dragging) {
+        ctx.beginPath();
+        ctx.moveTo(mousePressX, mousePressY);
+        ctx.lineTo(currentMouseX, currentMouseY);
+        ctx.strokeStyle = "blue";
+        ctx.stroke();
     }
 
     requestAnimationFrame(animate);
